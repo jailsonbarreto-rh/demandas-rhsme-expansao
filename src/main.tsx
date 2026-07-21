a22667023f1d9496c9b9b8c5ace84334372011cd
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-07-21 00:44:18 -0300

    style: load final brand placement refinements

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { App } from './App';
 import './responsive-modernization.css';
 import './index.css';
 import './brand.css';
+import './brand-overrides.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

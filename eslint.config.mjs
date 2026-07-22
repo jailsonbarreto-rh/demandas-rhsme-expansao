6a9a3d48689d50fca902ae0de451572961ea6cac
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-07-22 08:45:16 -0300

    chore: permitir variáveis descartadas explicitamente

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -39,6 +39,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unused-vars': ['error', {
         argsIgnorePattern: '^_',
         caughtErrorsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
       }],
     },
   },

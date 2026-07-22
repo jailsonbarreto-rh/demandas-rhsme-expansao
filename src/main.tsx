e35356aa7164a3b54c8acbb18c543113550d0c79
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-07-21 22:06:13 -0300

    fix: remover sobreposição visual legada da marca

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,9 @@ import { App } from './App';
 import './responsive-modernization.css';
 import './index.css';
 import './brand.css';
-import './brand-overrides.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
 );

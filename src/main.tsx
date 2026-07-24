1d24aae56fd3ede37f4ccaa80c86be06c3f3e806
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-07-24 19:20:42 -0300

    style: carregar refinamentos finais do R3

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './responsive-modernization.css';
 import './index.css';
 import './brand.css';
 import './minhas-demandas.css';
+import './r3-refinements.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

f15434ff1d2716f84c851a6a4646d5a84e359b6c
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-07-30 00:11:49 -0300

    feat: ampliar filtros temporais da carteira

--- src/filters/filterTypes.ts
+++ src/filters/filterTypes.ts
@@ -18,6 +18,10 @@ export interface QuickFilters {
   assinatura: boolean;
   hoje: boolean;
   vencido: boolean;
+  internoHoje: boolean;
+  internoVencido: boolean;
+  providenciaHoje: boolean;
+  providenciaVencida: boolean;
 }
 
 export const DEFAULT_DEMAND_FILTERS: DemandFilters = {
@@ -38,4 +42,8 @@ export const DEFAULT_QUICK_FILTERS: QuickFilters = {
   assinatura: false,
   hoje: false,
   vencido: false,
+  internoHoje: false,
+  internoVencido: false,
+  providenciaHoje: false,
+  providenciaVencida: false,
 };

b0c740e11e1b3c8b6df6778d262ccfca199231c0
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-07-30 14:00:13 -0300

    refactor: retirar restauração do contrato de demandas

--- src/services/contracts.ts
+++ src/services/contracts.ts
@@ -8,7 +8,6 @@ import type {
   PerfilMinimo,
   PerfilUsuario,
   ProgressInput,
-  RestoreDemandaInput,
   StatusTransitionInput,
 } from '../types';
 
@@ -25,7 +24,6 @@ export interface DemandasRepository {
   registerProgress(id: number, input: ProgressInput): Promise<void>;
   transitionStatus(id: number, input: StatusTransitionInput): Promise<void>;
   deleteLogically(id: number, input: DeleteDemandaInput): Promise<void>;
-  restore(id: number, input: RestoreDemandaInput): Promise<void>;
   subscribe(onRemoteChange: () => void): () => void;
 }
 

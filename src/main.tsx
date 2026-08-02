1873d3ed6439dbb751eb64409ef78c08f68ff6d7
Wilson Malafaia Peixoto <wilsonmp2@gmail.com>
2026-08-02 20:30:00 -0300

    refactor: share the root query client through the application context

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import '@fontsource-variable/inter';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { AppBootstrap } from './AppBootstrap';
-import { createAppQueryClient } from './query/queryClient';
+import { createAppQueryClient, DemandasQueryClientContext } from './query/queryClient';
 import './responsive-modernization.css';
 import './index.css';
 import './brand.css';
@@ -21,8 +21,10 @@ const queryClient = createAppQueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AppBootstrap />
-    </QueryClientProvider>
+    <DemandasQueryClientContext.Provider value={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <AppBootstrap />
+      </QueryClientProvider>
+    </DemandasQueryClientContext.Provider>
   </StrictMode>,
 );
